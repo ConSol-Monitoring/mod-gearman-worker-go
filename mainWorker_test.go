37fa1dcb0e0e3c8e7914956acafc0aed2672f7d5
Sven Nierlein <sven@nierlein.de>
2018-10-11 12:22:12 +0200

    fix load test

--- mainWorker_test.go
+++ mainWorker_test.go
@@ -17,23 +17,25 @@ func TestCheckLoads(t *testing.T) {
 		t.Errorf("loads are to ok, checkload says they are too hight")
 	}
 
-	config.loadLimit5 = 0.01
-	config.loadLimit15 = 0.01
+	config.loadLimit1 = 0.01
+	config.loadLimit5 = 999
+	config.loadLimit15 = 999
 
 	if mainworker.checkLoads() {
 		t.Errorf("load limit 1 exceeded")
 	}
 
-	config.loadLimit1 = 3
-	config.loadLimit5 = 1
-	config.loadLimit15 = 3
+	config.loadLimit1 = 999
+	config.loadLimit5 = 0.01
+	config.loadLimit15 = 999
 
 	if mainworker.checkLoads() {
 		t.Errorf("load limit 10 exceeded")
 	}
 
-	config.loadLimit5 = 3
-	config.loadLimit15 = 1
+	config.loadLimit1 = 999
+	config.loadLimit5 = 999
+	config.loadLimit15 = 0.01
 
 	if mainworker.checkLoads() {
 		t.Errorf("load limit 15 exceeded")
