70c606b7fbac69d3c51a6b402608fadd5d0d4aa8
Sven Nierlein <sven@consol.de>
2023-03-06 13:46:21 +0100

    cleanup tests
    use assert module

--- command_test.go
+++ command_test.go
@@ -2,99 +2,52 @@ package modgearman
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCommandParser_1(t *testing.T) {
 	cmdLine := "/bin/command"
 	cmd := parseCommand(cmdLine, &configurationStruct{})
-	if cmd.ExecType != Exec {
-		t.Errorf("exec type differs: %v vs. %v", cmd.ExecType, Exec)
-	}
-	if cmd.Command != cmdLine {
-		t.Errorf("command differs: %v vs. %v", cmd.Command, cmdLine)
-	}
-	if len(cmd.Args) != 0 {
-		t.Errorf("command args wrong")
-	}
-	if len(cmd.Env) != 0 {
-		t.Errorf("command env wrong")
-	}
+	assert.Equal(t, Exec, cmd.ExecType)
+	assert.Equal(t, "/bin/command", cmd.Command)
+	assert.Equal(t, []string{}, cmd.Args)
+	assert.Equal(t, map[string]string{}, cmd.Env)
 }
 
 func TestCommandParser_2(t *testing.T) {
 	cmdLine := "/bin/command args1"
 	cmd := parseCommand(cmdLine, &configurationStruct{})
-	if cmd.ExecType != Exec {
-		t.Errorf("exec type differs: %v vs. %v", cmd.ExecType, Exec)
-	}
-	if cmd.Command != "/bin/command" {
-		t.Errorf("command differs: %v vs. %v", cmd.Command, cmdLine)
-	}
-	if len(cmd.Args) != 1 {
-		t.Errorf("command args wrong")
-	}
-	if cmd.Args[0] != "args1" {
-		t.Errorf("command args differs: %v", cmd.Args)
-	}
-	if len(cmd.Env) != 0 {
-		t.Errorf("command env wrong")
-	}
+	assert.Equal(t, Exec, cmd.ExecType)
+	assert.Equal(t, "/bin/command", cmd.Command)
+	assert.Equal(t, []string{"args1"}, cmd.Args)
+	assert.Equal(t, map[string]string{}, cmd.Env)
 }
 
 func TestCommandParser_3(t *testing.T) {
 	cmdLine := `ENV1='var' ./bin/command "args 123" "TEST"`
 	cmd := parseCommand(cmdLine, &configurationStruct{})
-	if cmd.ExecType != Exec {
-		t.Errorf("exec type differs: %v vs. %v", cmd.ExecType, Shell)
-	}
-	if cmd.Command != "./bin/command" {
-		t.Errorf("command differs: %v vs. %v", cmd.Command, cmdLine)
-	}
-	if len(cmd.Args) != 2 {
-		t.Errorf("command args wrong")
-	}
-	if cmd.Args[0] != "args 123" {
-		t.Errorf("command args differs: %v", cmd.Args)
-	}
-	if len(cmd.Env) != 1 {
-		t.Errorf("command env wrong")
-	}
-	if cmd.Env["ENV1"] != "var" {
-		t.Errorf("command env differs: %v", cmd.Env)
-	}
+	assert.Equal(t, Exec, cmd.ExecType)
+	assert.Equal(t, "./bin/command", cmd.Command)
+	assert.Equal(t, []string{"args 123", "TEST"}, cmd.Args)
+	assert.Equal(t, map[string]string{"ENV1": "var"}, cmd.Env)
 }
 
 // unparsable trailing quotes -> shell
 func TestCommandParser_4(t *testing.T) {
 	cmdLine := `ENV1='var' ./bin/command "args 123" "TEST`
 	cmd := parseCommand(cmdLine, &configurationStruct{})
-	if cmd.ExecType != Shell {
-		t.Errorf("exec type differs: %v vs. %v", cmd.ExecType, Shell)
-	}
-	if cmd.Command != cmdLine {
-		t.Errorf("command differs: %v vs. %v", cmd.Command, cmdLine)
-	}
-	if len(cmd.Args) != 0 {
-		t.Errorf("command args wrong")
-	}
-	if len(cmd.Env) != 0 {
-		t.Errorf("command env wrong")
-	}
+	assert.Equal(t, Shell, cmd.ExecType)
+	assert.Equal(t, "/bin/sh", cmd.Command)
+	assert.Equal(t, []string{"-c", cmdLine}, cmd.Args)
+	assert.Equal(t, map[string]string{}, cmd.Env)
 }
 
 func TestCommandParser_5(t *testing.T) {
 	cmdLine := `trap 'echo Booh!' SIGINT SIGTERM; sleep 2`
 	cmd := parseCommand(cmdLine, &configurationStruct{})
-	if cmd.ExecType != Shell {
-		t.Errorf("exec type differs: %v vs. %v", cmd.ExecType, Shell)
-	}
-	if cmd.Command != cmdLine {
-		t.Errorf("command differs: %v vs. %v", cmd.Command, cmdLine)
-	}
-	if len(cmd.Args) != 0 {
-		t.Errorf("command args wrong")
-	}
-	if len(cmd.Env) != 0 {
-		t.Errorf("command env wrong")
-	}
+	assert.Equal(t, Shell, cmd.ExecType)
+	assert.Equal(t, "/bin/sh", cmd.Command)
+	assert.Equal(t, []string{"-c", cmdLine}, cmd.Args)
+	assert.Equal(t, map[string]string{}, cmd.Env)
 }
