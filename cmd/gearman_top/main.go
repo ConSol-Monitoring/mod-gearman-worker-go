ceeeee61dcf99f52f454c9317e2eeea9cca51e6d
Markus Kumpfmüller <markus.kumpfmueller@consol.de>
2024-05-03 15:40:59 +0200

    Create initial files for extension

---
+++ cmd/gearman_top/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"pkg/modgearman"
+)
+
+// Build contains the current git commit id
+// compile passing -ldflags "-X main.Build <build sha1>" to set the id.
+var Build string
+
+func main() {
+	// Demo code. Do not use
+	modgearman.Worker(Build)
+}
